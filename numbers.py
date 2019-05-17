9e3e74809a47e796db22617152d7feb2715bce92
binarybytes <31395956+binarybytes@users.noreply.github.com>
2019-05-16 22:03:12 -0400

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,12 @@
+
+##python numbers
+
+#an integer is a whole number; no decimals
+int1 = 10
+int2 = -4
+
+#a number with a decimal is a float
+int2 = .5
+
+#define a float using scientific notation, with e indicating the power of 10
+int3 = 3e2
