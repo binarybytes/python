86832e53f8fe5b81702f4dbbb4330350e95714d1
binarybytes <31395956+binarybytes@users.noreply.github.com>
2019-05-22 21:42:23 -0400

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,10 @@
+#lists in python are zero-indexed = meaning the first item in them sits at position 0
+my_list = ['a', 'b', 'c', 'd']
+
+my_list[0]
+
+#will print 'a'
+
+my_list[2]
+
+#will [print 'c'
