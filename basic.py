96ba9574e45a52d036965cf06f91a14aac1a8ac0
binarybytes <31395956+binarybytes@users.noreply.github.com>
2019-05-22 21:57:03 -0400

    Update basic.py

--- basic.py
+++ basic.py
@@ -7,4 +7,24 @@ my_list[0]
 
 my_list[2]
 
-#will [print 'c'
+#will print 'c'
+
+#tuples are a type of list that can't be changed once created
+tuple_of_numbers = (1, 2, 3, 4, 5)
+
+#dictionaries are a datatype that stores keys and values as '':''
+phone_contact= { 'mom': '416-123-4567', 'dad': '647-765-4321'}
+phone_contact['mom']
+#will print '416-123'4567'
+
+#control structures will let us execute code when the right conditions are met
+#2 types of control structures we'll use = if statements and 
+#if statements:
+number = 20
+if number > 10 #declare the if condition
+  print "thats a big number!"
+#this should pring "thats a big number!"
+
+#in if statements we'll use equality operators (greater than(>), less than (<), and greater than or equal to (>=),
+#and equal to (==), this is a double equals sign because a single equals sign (=), is an assignment operator.)
+
