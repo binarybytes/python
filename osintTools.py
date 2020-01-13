1d6d6da719cac26f73bd8a568d3a7e0970ba132d
binarybytes <31395956+binarybytes@users.noreply.github.com>
2020-01-12 19:43:28 -0500

    Update osintTools.py

--- osintTools.py
+++ osintTools.py
@@ -12,11 +12,18 @@ import requests
 from datetime import datetime
 from time import sleep
 
+print('.\n')
+print('.\n')
+print('.\n')
+print('.\n')
 #lame banner
 print('-' *50)
 print("starttime: "+str(datetime.now()))
 print('-'*50)
-
+print('.\n')
+print('.\n')
+print('.\n')
+print('.\n')
 #here is where u set the stage \(o_o)/
 #                                / \
 requests.urllib3.disable_warnings()
@@ -44,8 +51,10 @@ def parse_results(resp):
     scan_zults = resp['scans']
     #tell me wat i got back
     print('\nMalware Name, AV Name, Definitions Name, Last Updated\n')
+    print('endtime: 'str(datetime.now()))
   else: #otherwise, i guess iz gud in tha hood
-    print('nun found...')
+    print('nun found...\n')
+    print('endtime: '+str(datetime.now()))
     
 while True:
   hash = input('entur ur hash: \n')
