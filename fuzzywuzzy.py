a6f0ccf46cf8835ba7705b8dc7cb26d760b9667c
binarybytes <31395956+binarybytes@users.noreply.github.com>
2020-01-27 22:12:36 -0500

    Create fuzzywuzzy.py

---
+++ fuzzywuzzy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sockets
+
+
+s = sockets.socket(socket.AF_INET, socket.SOCK_STREAM)
+data = s.recv(1024(
+
+#test out the loop for getting and printing banners/reply
+def data:
+  print(s.recv(1024))
+   
+
+
+print("send bad bufferz")
+s.connect(('1.2.3.4', 110))  #connect to IP + POP3 port
+print(data) #get+print reply
+
+s.send('USER test' + '\r\n') #send uname test
+print(data) #get+print reply
+
+s.send('PASS test' +'\r\n' #send pwd test
+print(data) #get+print reply
+
+s.close()   #close socket
+print("done!" +\n) 
