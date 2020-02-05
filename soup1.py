cec087cedc904dd8be862e93c913a3b47805a05f
binarybytes <31395956+binarybytes@users.noreply.github.com>
2020-02-04 22:18:15 -0500

    Update soup1.py

--- soup1.py
+++ soup1.py
@@ -1,7 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+
 url =
 'https://www.crummy.com/software/BeautifulSoup/'
+
 r = requests.get(url)
+
 html_doc = r.text
+
 soup = BeautifulSoup(html_doc)
+
+for link in soup.find_all('a'):
+print(link.get('href'))
