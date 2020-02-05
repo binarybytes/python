74a3465f2884ec7d372ca2803d30bea0bbc76332
binarybytes <31395956+binarybytes@users.noreply.github.com>
2020-02-04 22:08:32 -0500

    Create soup.py

---
+++ soup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from urllib.request import urlretrieve
+import pandas as pd
+
+url='https://s3.amazonaws.com/assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
+
+#save>local
+urlretrieve(url, 'winequality-red.csv')
+
+#read>dataFrame+prnt(head)
+df = pd.read_csv('winequality-red.csv', sep=';')
+print(df.head())
