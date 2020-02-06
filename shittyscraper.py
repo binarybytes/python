2bd12135020743e4dba72d85e767cdab8fe9a1df
binarybytes <31395956+binarybytes@users.noreply.github.com>
2020-02-05 21:29:56 -0500

    Update shittyscraper.py

--- shittyscraper.py
+++ shittyscraper.py
@@ -27,28 +27,26 @@ print(pretty_soup)
 import requests
 from bs4 import BeautifulSoup
 
-# Specify url: url
+# Specify url
 url = 'https://www.python.org/~guido/'
 
 # Package the request, send the request and catch the response: r
 r = requests.get(url)
 
-# Extract the response as html: html_doc
+# Extracts the response as html: html_doc
 html_doc = r.text
 
-# Create a BeautifulSoup object from the HTML: soup
+# create a BeautifulSoup object from the HTML: soup
 soup = BeautifulSoup(html_doc)
 
-# Get the title of Guido's webpage: guido_title
-guido_title = soup.title
+# Print the title of Guido's webpage
+print(soup.title)
 
-# Print the title of Guido's webpage to the shell
-print(guido_title)
+# Find all 'a' tags (which define hyperlinks): a_tags
+a_tags = soup.find_all('a')
 
-# Get Guido's text: guido_text
-guido_text = soup.get_text()
-
-# Print Guido's text to the shell
-print(guido_text)
+# Print the URLs to the shell
+for link in a_tags:
+    print(link.get('href'))
 
 #https://www.datacamp.com/courses/importing-data-in-python-part-2
