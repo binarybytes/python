5f0e4ab53426da3b0ded4ba92f9919a41382c32d
binarybytes <31395956+binarybytes@users.noreply.github.com>
2024-02-22 19:38:54 -0500

    Update num.py

--- num.py
+++ num.py
@@ -2,8 +2,10 @@
 def every_three_nums(start):
   return list(range(start, 101, 3))
 print(every_three_nums(91))
+#returns [91, 94, 97, 100]
 
 #remove middle nums:
 def remove_middle(my_list, start, end):
   return my_list[:start] + my_list[end+1:]
 print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
+#returns [4, 23, 42] because elements at indices 1, 2, and 3 have been removed
