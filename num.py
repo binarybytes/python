168d1f54a558c76dc03338ac9b785f3dafc69f8c
binarybytes <31395956+binarybytes@users.noreply.github.com>
2024-02-22 21:50:10 -0500

    Update num.py

--- num.py
+++ num.py
@@ -34,3 +34,19 @@ def add_greetings(names):
     new.append('Hello, ' + x)
   return new
 print(add_greetings(["Owen", "Max", "Sophie"]))
+
+
+#large nums
+def larger_sum(lst1, lst2):
+  sum1 = 0
+  sum2 = 0
+  for number in lst1:
+    sum1 += number
+  for number in lst2:
+    sum2 += number
+  if sum1 >= sum2:
+    return lst1
+  else: 
+    return lst2
+
+print(larger_sum([1, 9, 5], [2, 3, 7]))
